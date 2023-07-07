f846c74ad1ef964e6dc3f4ae45c60cb8a628c6a6
syipmong <syipmong5@gmail.com>
2023-07-07 16:53:08 -0700

    test

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,49 +1,34 @@
-#include <stdio.h>
 #include "main.h"
 
 /**
- * binary_to_uint - Converts a binary number to an unsigned int
- * @b: The binary number as a string
- *
- * Return: The converted unsigned int
+ * binary_to_uint- convert binary to unsigned int
+ * @b: binary
+ * Return: unsigned int
  */
+
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int result = 0;
+
+	int night = 0, j;
+	unsigned int number = 0, non_binary = 0;
 
 	if (b == NULL)
-		return 0;
+		return (non_binary);
 
-	for (int i = 0; b[i] != '\0'; i++)
+	while (b[night] != '\0')
+		night++;
+	night -= 1;
+	j = 0;
+	while (b[j])
 	{
-		if (b[i] == '0')
-		{
-			result <<= 1; /* Left shift the result by 1 position */
-		}
-		else if (b[i] == '1')
-		{
-			result = (result << 1) | 1; /* Left shift the result by 1 position and set the rightmost bit to 1 */
-		}
-		else
-		{
-			return 0; /* Invalid character encountered, return 0 */
-		}
+		if ((b[j] != '0') && (b[j] != '1')) /*&& logical AND operator*/
+			return (non_binary);
+
+		if (b[j] == '1')
+			number += (1 * (1 << night)); /*<< bitwise left shift operator*/
+		j++;
+
+		night--;
 	}
-
-	return result;
+	return (number);
 }
-
-/**
- * main - Entry point
- *
- * Return: Always 0
- */
-int main(void)
-{
-	const char *binary = "110101";
-	unsigned int number = binary_to_uint(binary);
-	printf(" %s: %u\n", binary, number);
-
-	return 0;
-}
-
